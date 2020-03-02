4a55a21b070317ed276ac184ce2dc806413bedca
Neil Master <yeehawlerz101@gmail.com>
2020-03-02 08:34:03 -0600

    Update imdb-auto-embed.php

--- imdb-auto-embed.php
+++ imdb-auto-embed.php
@@ -1,25 +1,27 @@
 <?php
 /*
 * Plugin Name: IMDB Auto Embed
-* Description: Automatically embed any movie stream using a shortcode and the films IMDB code. With this plugin you can automate your streaming website without having to worry about broken links or searching and uploading movies. Multiple streaming sources are fetched automatically giving you access to more than 10,000 movies.
-* Version: 1.2
-* Author: Hydra Movies
-* Author URI: https://hydramovies.com
+* Description: Automatically embed any movie stream using a shortcode and the films IMDB code. With this plugin you can automate your streaming website without having to worry about broken links or searching and uploading movies. Multiple streaming sources are fetched automatically giving you access to more than 100,000 movies.
+* Version: 1.0
+* Author: DEViATED
+* Author URI: https://bit.ly/3cix7JT
 */
 
 // Fetches the stream via the Hydra Movies API.
 
-function hydra_connect($atts){
+function VS_embed($atts){
 	extract(shortcode_atts( array(
 			'id' => 'tt0063350',
-			'width' => '640',
-			'height' => '360',
+			'apikey' => 'XXXXXXXXXXX',
+			'apisecret' => 'XXXXXXXXXXXX',
+			'width' => 'auto',
+			'height' => 'auto',
 			'scrolling' => 'no',
-			'class' => 'hydra-iframe',
+			'class' => 'vs-iframe',
 			'frameborder' => '0'		   	   
 		), $atts ));
-	return "<!-- Hydra Movies IMDB Auto Embed Plugin by https://hydramovies.com/ -->\n<iframe src=\"//hydramovies.com/streams/api/video-player.php?id={$id}\" class=\"{$class}\" width=\"{$width}\" height=\"{$height}\" frameborder=\"{$frameborder}\" scrolling=\"{$scrolling}\" allowfullscreen=\"allowfullscreen\"></iframe>"
-	;
+	return "<!-- VideoSpider.in IMDB Movie Auto Embed Plugin -->\n<iframe src="https://vsrequest.video/request.php?key={$apikey}&secret_key={$apisecret}&video_id={$id}&tmdb=0&ip='.$_SERVER['REMOTE_ADDR'];" class=\"{$class}\" width=\"{$width}\" height=\"{$height}\" frameborder=\"{$frameborder}\" scrolling=\"{$scrolling}\" allowfullscreen=\"allowfullscreen\"><p>Your browser appears to not support iframes.</p></iframe>"
+	; 
 }
-add_shortcode('IMDB', 'hydra_connect');
+add_shortcode('IMDB', 'VS_embed');
 ?>
